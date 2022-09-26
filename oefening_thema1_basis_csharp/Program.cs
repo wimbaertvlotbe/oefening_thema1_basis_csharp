8b0b135aa256fb441fd410aa58c803b438d6da94
wimbaertvlotbe <50639680+wimbaert@users.noreply.github.com>
2022-09-26 09:15:48 +0200

    Add project files.

---
+++ oefening_thema1_basis_csharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace oefening_thema1_basis_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
